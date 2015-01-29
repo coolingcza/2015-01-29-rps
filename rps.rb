de45283b56694159ee1884bcc8e31dea1b9711f2
Crystal Cooling <cooling.cne@gmail.com>
2015-01-29 16:11:48 -0600

    Level 0.1, limit to valid entries.

--- rps.rb
+++ rps.rb
@@ -1,45 +1,47 @@
 "Rock, Paper, Scissors!"
-puts "Player 1, enter your play:"
-play1 = gets.chomp.downcase
-puts "Player 2, enter your play:"
-play2 = gets.chomp.downcase
 
 valid_entries = ["rock","paper","scissors"]
+
+puts "Player 1, enter your play:"
+play1 = gets.chomp.downcase
+
+until valid_entries.include? play1 do
+  puts "Entry was not rock, paper, or scissors.  Please try again:"
+  play1 = gets.chomp.downcase
+end
+
+puts "Player 2, enter your play:"
+play2 = gets.chomp.downcase
+until valid_entries.include? play2 do
+  puts "Entry was not rock, paper, or scissors.  Please try again:"
+  play2 = gets.chomp.downcase
+end
+
 
 points_p1 = 0
 points_p2 = 0
 
 game_val = 0
 
-v1 = valid_entries.include? play1
-v2 = valid_entries.include? play2
 
-if v1 && v2
-  if play1 == play2
-    game_val = 0
-  else
-    if play1 == "rock"
-      play2 == "scissors" ? game_val += 1 : game_val -= 1
-    elsif play1 == "paper"
-      play2 == "rock" ? game_val += 1 : game_val -= 1
-    else
-      play2 == "paper" ? game_val += 1 : game_val -= 1
-    end
-  end
-  # declare winner:
-  if game_val == 1
-    puts "Player 1 is the winner!"
-  elsif game_val == -1
-    puts "Player 2 is the winner!"
-  else
-    puts "It's a tie!"
-  end
+
+if play1 == play2
+  game_val = 0
 else
-  if !v1 && !v2
-    puts "Invalid entries."
-  elsif v2
-    puts "Invalid entry for Player 1"
+  if play1 == "rock"
+    play2 == "scissors" ? game_val += 1 : game_val -= 1
+  elsif play1 == "paper"
+    play2 == "rock" ? game_val += 1 : game_val -= 1
   else
-    puts "Invalid entry for Player 2"
+    play2 == "paper" ? game_val += 1 : game_val -= 1
   end
 end
+# declare winner:
+if game_val == 1
+  puts "Player 1 is the winner!"
+elsif game_val == -1
+  puts "Player 2 is the winner!"
+else
+  puts "It's a tie!"
+end
+
--- test.rb
+++ test.rb
@@ -3,13 +3,13 @@ play1=gets.chomp.downcase
 avail_play = ["rock","paper","scissors"]
 x=0
 
-until avail_play.include? play1 || x > 2
+until avail_play.include? play1 do
   puts "Entry was not rock, paper, or scissors.  Please try again:"
   play1 = gets.chomp.downcase
   x += 1
 end
 
-if x == 3
+if x > 2
   puts "fail"
 else
   puts "success"
