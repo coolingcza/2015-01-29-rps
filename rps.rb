59471ec653a2dd37616f29d416def9e7725ad2e9
Crystal Cooling <cooling.cne@gmail.com>
2015-01-29 16:39:47 -0600

    Player class and driver method

--- rps.rb
+++ rps.rb
@@ -1,47 +1,64 @@
+require "pry"
+
 "Rock, Paper, Scissors!"
 
-valid_entries = ["rock","paper","scissors"]
-
-puts "Player 1, enter your play:"
-play1 = gets.chomp.downcase
-
-until valid_entries.include? play1 do
-  puts "Entry was not rock, paper, or scissors.  Please try again:"
-  play1 = gets.chomp.downcase
-end
-
-puts "Player 2, enter your play:"
-play2 = gets.chomp.downcase
-until valid_entries.include? play2 do
-  puts "Entry was not rock, paper, or scissors.  Please try again:"
-  play2 = gets.chomp.downcase
-end
-
-
-points_p1 = 0
-points_p2 = 0
-
-game_val = 0
-
-
-
-if play1 == play2
-  game_val = 0
-else
-  if play1 == "rock"
-    play2 == "scissors" ? game_val += 1 : game_val -= 1
-  elsif play1 == "paper"
-    play2 == "rock" ? game_val += 1 : game_val -= 1
-  else
-    play2 == "paper" ? game_val += 1 : game_val -= 1
+class Player
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @valid_entries = ["rock","paper","scissors"]
   end
+  
+  def get_move
+    puts "#{@name}, enter rock, paper, or scissors:"
+    @move = gets.chomp.downcase
+    until check_move(@move) do
+      puts "Entry was not rock, paper, or scissors.  Please try again:"
+      @move = gets.chomp.downcase
+    end
+    @move
+  end
+  
+  def check_move(move)
+    @valid_entries.include? move
+  end
+
 end
-# declare winner:
-if game_val == 1
-  puts "Player 1 is the winner!"
-elsif game_val == -1
-  puts "Player 2 is the winner!"
-else
-  puts "It's a tie!"
+
+def play_game
+  puts "Enter first player's name:"
+  player1 = Player.new(gets.chomp)
+  puts "Enter second player's name:"
+  player2 = Player.new(gets.chomp)
+  
+  play1 = player1.get_move
+  play2 = player2.get_move
+  
+
+  game_val = 0
+
+  if play1 == play2
+    game_val = 0
+  else
+    if play1 == "rock"
+      play2 == "scissors" ? game_val += 1 : game_val -= 1
+    elsif play1 == "paper"
+      play2 == "rock" ? game_val += 1 : game_val -= 1
+    else
+      play2 == "paper" ? game_val += 1 : game_val -= 1
+    end
+  end
+  # declare winner:
+  if game_val == 1
+    puts "#{player1.name} is the winner!"
+  elsif game_val == -1
+    puts "#{player2.name} is the winner!"
+  else
+    puts "It's a tie!"
+  end
+  
 end
+
+binding.pry
 
