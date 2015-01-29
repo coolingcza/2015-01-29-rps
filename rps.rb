e96fb4f5dead678c2a7c0bff81519746f361480a
Crystal Cooling <cooling.cne@gmail.com>
2015-01-29 14:11:26 -0600

    Level 0, part 1: basic RPS

--- rps.rb
+++ rps.rb
@@ -4,19 +4,42 @@ play1 = gets.chomp.downcase
 puts "Player 2, enter your play:"
 play2 = gets.chomp.downcase
 
-val = 0
+valid_entries = ["rock","paper","scissors"]
 
-if play1 == "rock"
-  val +=1 if play2 == "scissors"
-elsif play1 == "paper"
-  val += 1 if play2 == "rock"
-elsif play1 == "scissors"
-  val += 1 if play2 == "paper"
+points_p1 = 0
+points_p2 = 0
+
+game_val = 0
+
+v1 = valid_entries.include? play1
+v2 = valid_entries.include? play2
+
+if v1 && v2
+  if play1 == play2
+    game_val = 0
+  else
+    if play1 == "rock"
+      play2 == "scissors" ? game_val += 1 : game_val -= 1
+    elsif play1 == "paper"
+      play2 == "rock" ? game_val += 1 : game_val -= 1
+    else
+      play2 == "paper" ? game_val += 1 : game_val -= 1
+    end
+  end
+  # declare winner:
+  if game_val == 1
+    puts "Player 1 is the winner!"
+  elsif game_val == -1
+    puts "Player 2 is the winner!"
+  else
+    puts "It's a tie!"
+  end
 else
-  puts "Invalid entry: #{play1}"
+  if !v1 && !v2
+    puts "Invalid entries."
+  elsif v2
+    puts "Invalid entry for Player 1"
+  else
+    puts "Invalid entry for Player 2"
+  end
 end
-
-puts val
-
-
-  
