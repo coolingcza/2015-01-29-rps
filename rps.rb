456f3f5e4b93c96d5e6a4814201911bbcb56d61c
Crystal Cooling <cooling.cne@gmail.com>
2015-01-29 17:09:07 -0600

    Level 0, part 3: best of number of rounds

--- rps.rb
+++ rps.rb
@@ -4,10 +4,12 @@ require "pry"
 
 class Player
   attr_reader :name
+  attr_reader :score
   
   def initialize(name)
     @name = name
     @valid_entries = ["rock","paper","scissors"]
+    @score = 0
   end
   
   def get_move
@@ -23,39 +25,73 @@ class Player
   def check_move(move)
     @valid_entries.include? move
   end
+  
+  def won_round
+    @score += 1
+  end
+    
 
 end
 
 def play_game
+  
+  puts "How many rounds?"
+  rounds = gets.chomp.to_i
+  
   puts "Enter first player's name:"
   player1 = Player.new(gets.chomp)
   puts "Enter second player's name:"
   player2 = Player.new(gets.chomp)
   
-  play1 = player1.get_move
-  play2 = player2.get_move
+  rnd = 0
+  
+  until rnd > rounds-1 do
+  
+    play1 = player1.get_move
+    play2 = player2.get_move
   
 
-  game_val = 0
-
-  if play1 == play2
     game_val = 0
-  else
-    if play1 == "rock"
-      play2 == "scissors" ? game_val += 1 : game_val -= 1
-    elsif play1 == "paper"
-      play2 == "rock" ? game_val += 1 : game_val -= 1
+
+    if play1 == play2
+      game_val = 0
     else
-      play2 == "paper" ? game_val += 1 : game_val -= 1
+      if play1 == "rock"
+        play2 == "scissors" ? game_val += 1 : game_val -= 1
+      elsif play1 == "paper"
+        play2 == "rock" ? game_val += 1 : game_val -= 1
+      else
+        play2 == "paper" ? game_val += 1 : game_val -= 1
+      end
     end
+    # declare winner:
+    if game_val == 1
+      puts "#{player1.name} wins!"
+      player1.won_round
+    elsif game_val == -1
+      puts "#{player2.name} wins!"
+      player2.won_round
+    else
+      puts "It's a tie!"
+    end
+    
+    rnd += 1
   end
-  # declare winner:
-  if game_val == 1
-    puts "#{player1.name} is the winner!"
-  elsif game_val == -1
-    puts "#{player2.name} is the winner!"
+  
+  if rounds > 1
+  
+    puts "#{player1.name}'s score: #{player1.score}"
+    puts "#{player2.name}'s score: #{player2.score}"
+  
+    if player1.score > player2.score
+      puts "#{player1.name} is the winner!"
+    elsif player1.score < player2.score
+      puts "#{player2.name} is the winner!"
+    else
+      puts "It's a tie."
+    end
+    
   else
-    puts "It's a tie!"
   end
   
 end
