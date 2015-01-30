eafb7acc1a1dda2fd4b3781f9383247b2a708a02
Crystal Cooling <cooling.cne@gmail.com>
2015-01-30 14:16:08 -0600

    Player class test file, 3 should fail, 1 should pass

---
+++ rps_test.rb
@@ -0,0 +1,44 @@
+require_relative "rps"
+require "minitest/autorun"
+
+class RPSTest < Minitest::Test
+  
+  #player = Player.new("Argus",{"rock": "scissors","paper": "rock","scissors": "paper"})
+  
+  def test_player_move_type
+    player = Player.new("Argus",{"rock": "scissors","paper": "rock","scissors": "paper"})
+    player.get_move
+    assert_kind_of(Array,player.move) #should fail
+  end
+  
+  def test_player_check_move
+    player = Player.new("Argus",{"rock": "scissors","paper": "rock","scissors": "paper"})
+    player.get_move
+    foo = player.check_move(player.move)
+    assert_equal(false, foo) #should fail
+  end
+
+  def test_player_won_round
+    player = Player.new("Argus",{"rock": "scissors","paper": "rock","scissors": "paper"})
+    player.won_round
+    assert_equal(2,player.score) #should fail
+  end
+  
+  def test_player_won_round_twice
+    player = Player.new("Argus",{"rock": "scissors","paper": "rock","scissors": "paper"})
+    player.won_round
+    player.won_round
+    assert_equal(2,player.score) #should succeed
+  end
+    
+  
+end
+
+# class EventTest < Minitest::Test
+#
+#   def test_event
+#     party = Event.new(["Bill","Charlie"],"Destination",35.4)
+#     assert_kind_of Array, party.attendees
+#   end
+#
+# end
